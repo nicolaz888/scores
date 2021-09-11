d4de200dfec779e49ed9d800a5a61abb897edac3
Nicolaz David <nicolaz888@hotmail.com>
2021-09-10 19:47:42 -0500

    [UPD] - all cohorts working together

--- util/constants.js
+++ util/constants.js
@@ -1,4 +1,20 @@
 const PROGRAM_FOUNDATIONS = "Foundations";
 const PROGRAM_SPECIALIZATIONS = "Specialization";
 
-module.exports = { PROGRAM_FOUNDATIONS, PROGRAM_SPECIALIZATIONS }
+const arrColumnIndex = [
+    { column: 'B', index: 1 },
+    { column: 'C', index: 2 },
+    { column: 'D', index: 3 },
+    { column: 'E', index: 4 },
+    { column: 'F', index: 5 },
+    { column: 'G', index: 6 },
+    { column: 'H', index: 7 },
+    { column: 'I', index: 8 },
+    { column: 'J', index: 9 },
+    { column: 'K', index: 10 },
+    { column: 'L', index: 11 },
+    { column: 'M', index: 12 },
+    { column: 'N', index: 13 }
+]
+
+module.exports = { PROGRAM_FOUNDATIONS, PROGRAM_SPECIALIZATIONS, arrColumnIndex }
